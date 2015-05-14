4146cd51dc57366cd2659b1d2e3b998e824b7f60
O <lesson6@126.com>
2015-05-14 13:53:21 +0800

    MyIdentity:用户和角色

--- WebApplication37/App_Start/IdentityConfig.cs
+++ WebApplication37/App_Start/IdentityConfig.cs
@@ -106,4 +106,18 @@ namespace WebApplication37
             return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
         }
     }
+
+    // 角色管理
+    public class ApplicationRoleManager : RoleManager<IdentityRole>
+    {
+        public ApplicationRoleManager(IRoleStore<IdentityRole, string> roleStore)
+            : base(roleStore)
+        {
+        }
+
+        public static ApplicationRoleManager Create(IdentityFactoryOptions<ApplicationRoleManager> options, IOwinContext context)
+        {
+            return new ApplicationRoleManager(new RoleStore<IdentityRole>(context.Get<ApplicationDbContext>()));
+        }
+    }
 }
---
+++ WebApplication37/Models/MyIdentityModels.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace WebApplication37.Models
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "角色名")]
+        public string Name { get; set; }
+    }
+
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+
+        [DisplayName("员工编号")]
+        public string UserName { get; set; }
+
+        public IEnumerable<SelectListItem> RolesList { get; set; }
+    }
+
+    //显示用户和角色列表
+    public class ListUserAndRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+
+    // 显示用户名称
+    public class ListUser
+    {
+        public string Id { get; set; }
+        [DisplayName("用户编号")]
+        public string EmployeeId { get; set; }
+        [DisplayName("用户名")]
+        public string EmployeeName { get; set; }
+        [DisplayName("锁定")]
+        public bool LockoutEnabled { get; set; }
+        [DisplayName("锁定时间")]
+        public DateTime? LockoutEndDateUtc { get; set; }
+        [DisplayName("错误尝试")]
+        public int AccessFailedCount { get; set; }
+    }
+}
